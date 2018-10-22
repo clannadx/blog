5011a6823ebc5c05062e5a3e3fafbdcdc75355e7
clannadx <xxi124@sina.com>
2018-10-22 12:35:04 +0800

    1

--- db/blog.js
+++ db/blog.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('blog', 'root', 'saber', {
+const sequelize = new Sequelize('blog', 'root', '', {
     host: 'localhost',
     port: 3306,
     dialect: 'mysql',
